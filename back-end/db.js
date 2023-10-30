6ac0576a8afaa8fbb8d443854e4d6cc0f9517569
Jose Cardona <jpcardonamena@gmail.com>
2023-10-30 15:19:18 +0100

    Added documentation

--- back-end/db.js
+++ back-end/db.js
@@ -1,12 +1,41 @@
+/**
+ * Database Connection Module
+ *
+ * This module establishes a connection to the PostgreSQL database using the `pg` library.
+ * It exports a pool object that can be used to interact with the database.
+ *
+ * @module databaseConnection
+ *
+ * @requires pg
+ *
+ */
+
 const Pool = require("pg").Pool;
 
-const pool= new Pool({
+/**
+ * Configuration object for the PostgreSQL connection pool.
+ *
+ * @typedef {Object} PoolConfig
+ * @property {string} user - The PostgreSQL user.
+ * @property {string} password - The password for the PostgreSQL user.
+ * @property {string} host - The host of the PostgreSQL server.
+ * @property {number} port - The port of the PostgreSQL server.
+ * @property {string} database - The name of the database to connect to.
+ */
+
+/**
+ * The connection pool object.
+ *
+ * @type {Pool}
+ * @const
+ * @namespace pool
+ */
+const pool = new Pool({
     user: "postgres",
     password: "Cardona8399!",
     host: "localhost",
     port: 5432,
     database: "applicants-db"
-})
+});
 
 module.exports = pool;
-
--- back-end/index.js
+++ back-end/index.js
@@ -1,16 +1,39 @@
+/**
+ * CRUD Operations for Applicants
+ *
+ * This module provides the CRUD (Create, Read, Update, Delete) operations for managing applicants.
+ * It uses Express.js for handling HTTP requests and `pg` for interacting with the PostgreSQL database.
+ *
+ * @module CRUDApplicants
+ *
+ * @requires express
+ * @requires cors
+ * @requires db
+ *
+ */
+
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
 
-//Middleware
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-
-//ROUTES//
-
-//Create an applicant
+/**
+ * Create an applicant.
+ *
+ * This endpoint allows for the creation of a new applicant in the database.
+ *
+ * @name CreateApplicant
+ * @route {POST} /applicants
+ * @bodyparam {string} first_name - The first name of the applicant.
+ * @bodyparam {string} last_name - The last name of the applicant.
+ * @bodyparam {string} email - The email address of the applicant.
+ * @bodyparam {string} position - The position applied for by the applicant.
+ * @bodyparam {boolean} liked - Whether the applicant was liked or not.
+ */
 app.post("/applicants", async (req, res) => {
     try{
         const {first_name, last_name, email, position, liked } = req.body;
@@ -19,20 +42,33 @@ app.post("/applicants", async (req, res) => {
     }
     catch (err){
         console.log(err.message)
-    }
-})
+    }});
 
-//Get all applicants
+/**
+ * Get all applicants.
+ *
+ * This endpoint retrieves all the applicants from the database.
+ *
+ * @name GetAllApplicants
+ * @route {GET} /applicants
+ */
 app.get("/applicants", async(req,res) => {
     try {
         const allApplicants = await pool.query("SELECT * FROM applicants");
         res.json(allApplicants.rows)
     } catch (err) {
         console.log(err.message);
-    }
-})
+    }});
 
-//Get a specific applicant
+/**
+ * Get a specific applicant.
+ *
+ * This endpoint retrieves a specific applicant based on their ID.
+ *
+ * @name GetApplicant
+ * @route {GET} /applicants/:id
+ * @routeparam {number} id - The ID of the applicant.
+ */
 app.get("/applicants/:id" , async (req, res) => {
     try {
         const {id} = req.params;
@@ -41,10 +77,22 @@ app.get("/applicants/:id" , async (req, res) => {
         res.json(applicant.rows[0]);
     } catch (err) {
         console.log(err.message);
-    }
-})
+    }});
 
-//update an applicant
+/**
+ * Update an applicant.
+ *
+ * This endpoint updates the details of a specific applicant based on their ID.
+ *
+ * @name UpdateApplicant
+ * @route {PUT} /applicants/:id
+ * @routeparam {number} id - The ID of the applicant.
+ * @bodyparam {string} first_name - The updated first name of the applicant.
+ * @bodyparam {string} last_name - The updated last name of the applicant.
+ * @bodyparam {string} email - The updated email address of the applicant.
+ * @bodyparam {string} position - The updated position of the applicant.
+ * @bodyparam {boolean} liked - The updated liked status of the applicant.
+ */
 app.put("/applicants/:id", async (req, res) => {
     try {
         const {id} = req.params;
@@ -57,22 +105,31 @@ app.put("/applicants/:id", async (req, res) => {
     } catch (err) {
         console.log(err.message);
     }
-})
+});
 
-//delete an applicant
+/**
+ * Delete an applicant.
+ *
+ * This endpoint deletes a specific applicant from the database based on their ID.
+ *
+ * @name DeleteApplicant
+ * @route {DELETE} /applicants/:id
+ * @routeparam {number} id - The ID of the applicant.
+ */
 app.delete("/applicants/:id", async (req, res) => {
-    try {
-        const {id} = req.params;
-        const deleteApplicant = await pool.query("DELETe FROM applicants WHERE applicant_id = $1", [id]);
+    app.delete("/applicants/:id", async (req, res) => {
+        try {
+            const {id} = req.params;
+            const deleteApplicant = await pool.query("DELETe FROM applicants WHERE applicant_id = $1", [id]);
 
-        res.json("Applicant deleted successfully!");
-    } catch (err) {
-        console.log(err.message);
-    }
-})
+            res.json("Applicant deleted successfully!");
+        } catch (err) {
+            console.log(err.message);
+        }
+    })
+});
 
-//start the server
-
-app.listen(8000, () =>{
+// Start the server
+app.listen(8000, () => {
     console.log("server has started on port 8000.")
-})
+});
